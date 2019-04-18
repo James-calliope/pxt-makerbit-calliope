aa2eb37c36bf1aa826586fb96a35b9e79dd1654e
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2019-04-18 21:51:47 +0200

    removed tests

--- test.ts
+++ test.ts
@@ -1,33 +1,3 @@
 /**
  * Helper tests
  */
-
-makerbit.setLedPins(0);
-makerbit.setDigitalPin(5, 1);
-makerbit.setAnalogPin(5, 1023);
-let level: number = makerbit.level(PinLevel.High);
-
-/**
- * LCD tests
- */
-
-makerbit.connectLcd(39);
-makerbit.showStringOnLcd("Hello world", 0, 15);
-makerbit.showNumberOnLcd(42, 16, 2);
-makerbit.clearLcd();
-makerbit.setLcdBacklight(LcdBacklight.On);
-const pos: number = makerbit.position(LcdPosition.P0);
-const isLcdConnected: boolean = makerbit.isLcdConnected();
-
-/**
- * IR tests
- */
-
-makerbit.connectInfrared(MakerBitPin.A0);
-makerbit.onIrButtonPressed(IrButton.Ok, () => { });
-makerbit.onIrButtonReleased(IrButton.Up, () => { });
-makerbit.isIrButtonPressed(IrButton.Number_0);
-makerbit.onIrCommandReceived(() => { });
-makerbit.onIrCommandExpired(() => { });
-const command: number = makerbit.irCommandCode();
-const button: number = makerbit.irButton(IrButton.Number_9);
